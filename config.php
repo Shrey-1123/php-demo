c5dcd0453ff129cbee0a891ad7d9d8543bdd9f12
Shrey Kumar <49495667+Shrey-1123@users.noreply.github.com>
2020-07-19 17:56:14 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,15 @@
 <?php
-   define('DB_SERVER', 'localhost');
+   /*define('DB_SERVER', 'localhost');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', '');
    define('DB_DATABASE', 'trainingproject');
+   $con = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE)
+         or
+         die(mysqli_error($con));*/
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'id14375167_phpdemoshrey');
+   define('DB_PASSWORD', '@Lumos0123456789');
+   define('DB_DATABASE', 'id14375167_trainingproject');
    $con = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE)
          or
          die(mysqli_error($con));
